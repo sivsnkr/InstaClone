7a0e63d37ae8d31f372bcf71b56357f3d89d50dd
ced17i026 <ced17i026@iiitdm.ac.in>
2019-01-26 18:21:28 +0530

    add reudcer for posts

--- client/src/store/reducer/index.js
+++ client/src/store/reducer/index.js
@@ -1,8 +1,10 @@
 import {combineReducer} from "redux";
 import user from "./currentUser";
 import error from "./error";
+import post from "./post";
 
 export default combineReducer({
     user,
+    post,
     error, 
 });
---
+++ client/src/store/reducer/post.js
@@ -0,0 +1,21 @@
+import {ADD_POST,REMOVE_POST} from "../actionTypes";
+const defaultState =[{
+    id:null,
+    url:null
+}]
+export default (state=defaultState,action)=>{
+    switch(action.types){
+        case ADD_POST:
+            const posts = action.posts.map(post=>{
+                return{
+                    id: post._id,
+                    url: post.url,
+                }
+            })
+            return [...state,...posts];
+        case REMOVE_POST:
+            return state.filter(post=>post.id!=action.id);
+        default:
+            return state;
+    }
+}

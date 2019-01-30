86acecebcadbd5352a5492fc436a26077bb760dd
ced17i026 <ced17i026@iiitdm.ac.in>
2019-01-30 21:07:44 +0530

    add appropriate error handler for handling error if server not able to insert message

--- client/src/store/actions/message.js
+++ client/src/store/actions/message.js
@@ -1,9 +1,15 @@
 import {apiCall} from "../../services/api";
-
+import {addError,removeError} from "../actions/error";
+import {store} from "../../containers/App";
 export const addMessage = function(id,data){
     return ()=>{
         return new Promise((resolve,reject)=>{
-            return apiCall("post",`/api/user/${id}/new_message`,data);
+            return apiCall("post",`/api/user/${id}/new_message`,data)
+            .then(res=>{
+                store.dispatch(removeError());
+            }).catch(err=>{
+                store.dispatch(addError());
+            })
         })
     }
 }
